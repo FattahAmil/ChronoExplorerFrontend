94666430cb2fb05333be933aa3b29e8a68c7c381
abdelfettahamil <fattah.ptech2020@gmail.com>
2025-05-15 13:53:38 +0200

    setup le projet frontend

---
+++ src/app/media/media-gallery/media-gallery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-gallery',
+  imports: [],
+  templateUrl: './media-gallery.component.html',
+  styleUrl: './media-gallery.component.scss'
+})
+export class MediaGalleryComponent {
+
+}
